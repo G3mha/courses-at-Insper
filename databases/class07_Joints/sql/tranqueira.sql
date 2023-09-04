12490875bf8a5687900a3131aa6516ceda98eaa3
Enricco Gemha <riccogemha@gmail.com>
2023-09-04 08:46:05 -0300

    class07 - begins

---
+++ databases/class07_Joints/sql/tranqueira.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS tranqueira;
+CREATE DATABASE tranqueira;
+USE tranqueira;
+
+CREATE TABLE comida (
+	id INT NOT NULL AUTO_INCREMENT,
+	Nome VARCHAR(30),
+    idPerigo INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE perigo (
+	id INT NOT NULL AUTO_INCREMENT,
+    Nome VARCHAR(20),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE comida ADD FOREIGN KEY (idPerigo) REFERENCES perigo (id);
+
+INSERT INTO perigo VALUES (1, 'Cardiaco'), (2, 'Intestinal'), (3, 'Dermatologico'), (4, 'Mental');
+INSERT INTO comida VALUES (1, 'Torresmo', 1), (2, 'Alface', NULL), (3, 'Coxinha', 2), (4, 'Espetinho', 2);
+
+SELECT * FROM comida;
+SELECT * FROM perigo;
