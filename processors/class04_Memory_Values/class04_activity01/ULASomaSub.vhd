7929eeb0cde963d3138d3adcefaec56e555e2a3f
Enricco Gemha <riccogemha@gmail.com>
2023-09-07 23:39:27 -0300

    kickstart class04 - PROCESSORS

--- processors/class04_Memory_Values/class04_activity01/ULASomaSub.vhd
+++ processors/class04_Memory_Values/class04_activity01/ULASomaSub.vhd
@@ -3,10 +3,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;    -- Biblioteca IEEE para funções aritméticas
 
 entity ULASomaSub is
-    generic ( larguraDados : natural := 4 );
+    generic ( larguraDados : natural := 8 );
     port (
       entradaA, entradaB:  in STD_LOGIC_VECTOR((larguraDados-1) downto 0);
-      seletor:  in STD_LOGIC;
+      seletor:  in STD_LOGIC_VECTOR(1 downto 0);
       saida:    out STD_LOGIC_VECTOR((larguraDados-1) downto 0)
     );
 end entity;
@@ -14,8 +14,12 @@ end entity;
 architecture comportamento of ULASomaSub is
    signal soma :      STD_LOGIC_VECTOR((larguraDados-1) downto 0);
    signal subtracao : STD_LOGIC_VECTOR((larguraDados-1) downto 0);
+   signal passa :     STD_LOGIC_VECTOR((larguraDados-1) downto 0);
     begin
       soma      <= STD_LOGIC_VECTOR(unsigned(entradaA) + unsigned(entradaB));
       subtracao <= STD_LOGIC_VECTOR(unsigned(entradaA) - unsigned(entradaB));
-      saida <= soma when (seletor = '1') else subtracao;
+      passa     <= STD_LOGIC_VECTOR(unsigned(entradaB));
+      saida     <= soma      when (seletor = '01') else 
+                   subtracao when (seletor = '00') else
+                   passa;
 end architecture;
--- processors/class04_Memory_Values/class04_activity01/decoderGeneric.vhd
+++ processors/class04_Memory_Values/class04_activity01/decoderGeneric.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity decoderGeneric is
   port ( entrada : in std_logic_vector(3 downto 0);
-         saida : out std_logic_vector(3 downto 0)
+         saida : out std_logic_vector(5 downto 0)
   );
 end entity;
 
@@ -13,13 +13,15 @@ architecture comportamento of decoderGeneric is
   constant LDA  : std_logic_vector(3 downto 0) := "0001";
   constant SOMA : std_logic_vector(3 downto 0) := "0010";
   constant SUB  : std_logic_vector(3 downto 0) := "0011";
-  constant CLRA : std_logic_vector(3 downto 0) := "1111";
+  constant LDI  : std_logic_vector(3 downto 0) := "0100";
+  constant STA  : std_logic_vector(3 downto 0) := "0101";
 
   begin
-saida <= "0000" when entrada = NOP else
-         "0100" when entrada = LDA else
-         "1101" when entrada = SOMA else
-         "1100" when entrada = SUB else
-         "1011" when entrada = CLRA else
-         "0000";  -- NOP para os entradas Indefinidas
+saida <= "000000" when entrada = NOP else
+         "011010" when entrada = LDA else
+         "010110" when entrada = SOMA else
+         "010010" when entrada = SUB else
+         "111000" when entrada = LDI else
+         "001001" when entrada = STA else
+         "000000";  -- NOP para os entradas Indefinidas
 end architecture;
