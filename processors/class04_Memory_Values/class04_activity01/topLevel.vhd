608bea2e2b350642cc58b4029a34762b86a3521a
Enricco Gemha <riccogemha@gmail.com>
2023-09-12 11:28:57 -0300

    acho que finalizei o circuito

--- processors/class04_Memory_Values/class04_activity01/topLevel.vhd
+++ processors/class04_Memory_Values/class04_activity01/topLevel.vhd
@@ -3,10 +3,11 @@ use ieee.std_logic_1164.all;
 
 entity topLevel is
   -- Total de bits das entradas e saidas
-  generic ( larguraDados      : natural := 8; -- 2^8 = 256 valores
-            larguraEnderecos  : natural := 9; -- 2^9 = 512 enderecos
-            larguraInstrucoes : natural := 13; -- opCode | acessa memória? | endereço/valor
-					  simulacao         : boolean := TRUE -- para gravar na placa, altere de TRUE para FALSE
+  generic ( larguraDados            : natural := 8; -- 2^8 = 256 valores
+            larguraEnderecos        : natural := 9; -- 2^9 = 512 enderecos
+            larguraInstrucoes       : natural := 13; -- opCode | acessa memória? | endereço/valor
+					  larguraSinaisDeControle : natural := 6; -- SelMux | Habilita_A | Operacao_ULA_2_bits | habLeituraMEM | habEscritaMEM
+            simulacao               : boolean := TRUE -- para gravar na placa, altere de TRUE para FALSE
   );
   port   (
     CLOCK_50 : in std_logic;
@@ -21,17 +22,23 @@ end entity;
 architecture arquitetura of topLevel is
 
   -- Instruction
-  signal instruction      : std_logic_vector (12 downto 0);
-  alias opCode            : std_logic_vector (3 downto 0) is Instrucao(12 downto 9);
-  alias RAM_addr          : std_logic_vector (8 downto 0) is Instrucao(8 downto 0);
-  alias instruction_value : std_logic_vector (7 downto 0) is Instrucao(7 downto 0);
+  signal instruction   : std_logic_vector (12 downto 0);
+  alias opCode         : std_logic_vector (3 downto 0) is instruction(12 downto 9);
+  alias imediato_addr  : std_logic_vector (8 downto 0) is instruction(8 downto 0);
+  alias imediato_value : std_logic_vector (7 downto 0) is instruction(7 downto 0);
+
+  -- Decoder
+  signal Sinais_Controle : std_logic_vector (larguraSinaisDeControle - 1 downto 0);
 
   -- MUX
   signal SelMUX  : std_logic;
   signal MUX_out : std_logic_vector (larguraDados-1 downto 0);
   
   -- RAM
-  signal RAM_out : std_logic_vector (larguraDados-1 downto 0);
+  signal RAM_out       : std_logic_vector (larguraDados-1 downto 0);
+  alias RAM_Habilita   : std_logic_vector is instruction(8);
+  signal habEscritaMEM : std_logic;
+  signal habLeituraMEM : std_logic;
 
   -- RegA
   signal Habilita_A : std_logic;
@@ -39,19 +46,12 @@ architecture arquitetura of topLevel is
   signal RegA_out   : std_logic_vector (larguraDados-1 downto 0);
 
   -- PC
-  signal proxPC   : std_logic_vector (8 downto 0);
-  signal Endereco : std_logic_vector (8 downto 0);
+  signal proxPC   : std_logic_vector (larguraEnderecos-1 downto 0);
+  signal Endereco : std_logic_vector (larguraEnderecos-1 downto 0);
 
   -- ULA
   signal ULA_out       : std_logic_vector (larguraDados-1 downto 0);
   signal ULA_operation : std_logic_vector (1 downto 0);
-
-  signal chavesX_ULA_B : std_logic_vector (larguraDados-1 downto 0);
-  signal REG1_ULA_A : std_logic_vector (larguraDados-1 downto 0);
-  signal Saida_ULA : std_logic_vector (larguraDados-1 downto 0);
-  signal Sinais_Controle : std_logic_vector (3 downto 0);
-  signal Chave_Operacao_ULA : std_logic;
-  signal Reset_A : std_logic;
 
 begin
 
@@ -65,53 +65,54 @@ detectorSub0: work.edgeDetector(bordaSubida)
         port map (clk => CLOCK_50, entrada => (not KEY(0)), saida => CLK);
 end generate;
 
--- O port map completo do MUX.
+-- O port map completo do MUX. (DONE)
 MUX1 :  entity work.muxGenerico2x1  generic map (larguraDados => larguraDados)
         port map(entradaA_MUX => RAM_out,
                  entradaB_MUX => instruction_value,
                  seletor_MUX  => SelMUX,
                  saida_MUX    => MUX_out);
 
--- O port map completo do Acumulador.
+-- O port map completo do Acumulador. (DONE)
 REGA : entity work.registradorGenerico  generic map (larguraDados => larguraDados)
-          port map (DIN => ULA_out, bDOUT => RegA_out, ENABLE => Habilita_A, CLK => CLK, RST => '0');
+          port map (DIN => ULA_out, DOUT => RegA_out, ENABLE => Habilita_A, CLK => CLK, RST => '0');
 
--- O port map completo do Program Counter.
+-- O port map completo do Program Counter. (DONE)
 PC : entity work.registradorGenerico  generic map (larguraDados => larguraEnderecos)
           port map (DIN => proxPC, DOUT => Endereco, ENABLE => '1', CLK => CLK, RST => '0');
 
 incrementaPC :  entity work.somaConstante  generic map (larguraDados => larguraEnderecos, constante => 1)
         port map (entrada => Endereco, saida => proxPC);
 
--- O port map completo da ULA:
+-- o port map completo da memoria RAM (DONE)
+RAM1 : entity work.memoriaRAM   generic map (dataWidth => larguraDados, addrWidth => larguraEnderecos)
+          port map (addr => imediato_value, we => habEscritaMEM, re => habLeituraMEM, habilita  => RAM_Habilita, dado_in => RegA_out, dado_out => RAM_out, clk => CLK);
+
+-- O port map completo da ULA: (DONE)
 ULA1 : entity work.ULASomaSub  generic map(larguraDados => larguraDados)
           port map (entradaA => RegA_out, entradaB => MUX_out, saida => ULA_out, seletor => ULA_operation);
 
--- O port map completo da memória:
-ROM1 : entity work.memoriaROM  generic map (dataWidth => larguraDados, addrWidth => larguraEnderecos)
+-- O port map completo da memória: (DONE)
+ROM1 : entity work.memoriaROM  generic map (dataWidth => larguraInstrucoes, addrWidth => larguraEnderecos)
           port map (Endereco => Endereco, Dado => instruction);
 
--- O port map completo do Decoder:
-DEC : entity work.decoderGeneric  port map (entrada => Instrucoes, saida => Sinais_Controle);
+-- O port map completo do Decoder: (DONE)
+DEC : entity work.decoderGeneric  port map (entrada => opCode, saida => Sinais_Controle);
 
+-- (DONE)
+habEscritaMEM <= Sinais_Controle(0);
+habLeituraMEM <= Sinais_Controle(1);
+ULA_operation <= Sinais_Controle(3 downto 2);
+Habilita_A <= Sinais_Controle(4);
+SelMUX <= Sinais_Controle(5);
 
-selMUX <= Sinais_Controle(3);
-Habilita_A <= Sinais_Controle(2);
-Reset_A <= Sinais_Controle(1);
-Operacao_ULA <= Sinais_Controle(0);
-
--- I/O
-chavesY_MUX_A <= SW(3 downto 0);
-chavesX_ULA_B <= SW(9 downto 6);
-
--- A ligacao dos LEDs:
+-- A ligacao dos LEDs: (DONE)
 LEDR (9) <= SelMUX;
 LEDR (8) <= Habilita_A;
-LEDR (7) <= Reset_A;
-LEDR (6) <= Operacao_ULA;
-LEDR (5) <= '0';    -- Apagado.
-LEDR (4) <= '0';    -- Apagado.
-LEDR (3 downto 0) <= REG1_ULA_A;
+LEDR (7) <= ULA_operation(1);
+LEDR (6) <= ULA_operation(0);
+LEDR (5) <= habLeituraMEM;
+LEDR (4) <= habEscritaMEM;
+LEDR (3 downto 0) <= RegA_out;
 
 PC_OUT <= Endereco;
 
