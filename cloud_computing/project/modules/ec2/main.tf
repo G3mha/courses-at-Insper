fd261d54d7b8cce030b910ec75e0983bf37e1fa0
Enricco Gemha <riccogemha@gmail.com>
2023-11-19 21:46:28 -0300

    Update EC2 module with launch template and key pair configurations

--- cloud_computing/project/modules/ec2/main.tf
+++ cloud_computing/project/modules/ec2/main.tf
@@ -6,41 +6,39 @@ provider "aws" {
 
 module "security_group" {
   source = "./security_group"
-  # Parâmetros do módulo Security Group
 }
 
-resource "aws_launch_configuration" "my_launch_config" {
-  name_prefix                 = var.launch_config_name
+resource "tls_private_key" "enricco_key_pair" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "enricco_aws_key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.enricco_key_pair.public_key_openssh
+  depends_on = [ tls_private_key.enricco_key_pair ]
+}
+
+resource "aws_launch_template" "enricco_launch_template" {
+  name_prefix                 = var.launch_template_name
   image_id                    = var.ami_id
   instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = var.security_groups
+  key_name                    = aws_key_pair.enricco_aws_key_pair.key_name
+  vpc_security_group_ids      = [module.security_group.OW_security_group_id]
   user_data                   = var.user_data
-  lifecycle {
-    create_before_destroy = true
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "enricco_launch_template"
+    }
   }
 }
 
-resource "aws_autoscaling_group" "my_asg" {
+resource "aws_autoscaling_group" "enricco_asg" {
   desired_capacity     = var.desired_capacity
   max_size             = var.max_size
   min_size             = var.min_size
-  vpc_zone_identifier  = var.subnets
-  launch_configuration = aws_launch_configuration.my_launch_config.id
-}
-
-
-resource "aws_autoscaling_group" "" {
-  desired_capacity     = 1
-  max_size             = 1
-  min_size             = 1
-  vpc_zone_identifier  = [var.private_sub1_id, var.private_sub2_id]  # Assuming these are your private subnets
-
-   # Connect to the target group
-  target_group_arns = [var.target_group_arn]
   launch_template {
-    id = aws_launch_template.ec2_launch_templ.id
-    version = "$Latest"  # Use specific version or $Latest for the latest version of the template
+    id      = aws_launch_template.enricco_launch_template.id
+    version = "$Latest"
   }
-
 }
--- cloud_computing/project/modules/ec2/variables.tf
+++ cloud_computing/project/modules/ec2/variables.tf
@@ -2,31 +2,38 @@
 
 variable "region" {
   description = "AWS region"
-  
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "launch_config_name" {
+variable "launch_template_name" {
   description = "Name for the launch configuration"
+  type        = string
+  default     = "enricco_launch_template"
 }
 
 variable "ami_id" {
   description = "ID of the Amazon Machine Image (AMI)"
+  type        = string
+  default     = "ami-0d86d19bb909a6c95"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "key_name" {
   description = "Name of the key pair to use for the instance"
-}
-
-variable "security_groups" {
-  description = "List of security group IDs for the instance"
+  type        = string
+  default     = "enricco_key_pair"
 }
 
 variable "user_data" {
   description = "User data script for configuring the instance"
+  type        = string
+  default     = base64encode("echo 'Hello, World!' > index.html && nohup python -m SimpleHTTPServer 80 &")
 }
 
 variable "desired_capacity" {
@@ -45,10 +52,4 @@ variable "max_size" {
   description = "Maximum number of instances in the Auto Scaling Group"
   type        = number
   default     = 4
-}
-
-variable "subnets" {
-  description = "List of subnet IDs for the Auto Scaling Group"
-  type        = list(string)
-  default     = []
 }
